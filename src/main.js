cd50dd87155697bc75c6786a57dfaee4040896c9
Jenna Koslowski <jenna.koslowski@yahoo.com>
2020-07-04 20:29:36 -0400

    begin fetch of api

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 const buttons = document.getElementById('buttons');
 
+/* const requestURL = 'https://api.adviceslip.com/advice';
+const request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+ buttons.onclick = function giveAdvice() {
+  const advice = request.response;
+  const adviceSpace = document.createElement('p');
+  adviceSpace.textContent = advice['slip.advice'];
+  adviceSpace.appendChild(adviceSpace);
+}; */
+
 buttons.onclick = function giveAdvice() {
-  alert('hello');
+  const data = {};
+  fetch('https://api.adviceslip.com/advice', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  }).then((res) => res.json()).then((datas) => console.log(datas));
 };
