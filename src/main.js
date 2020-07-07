e321e4a0f0e3ef2ffa95d3bd5ac497cb2fa56069
Jenna Koslowski <jenna.koslowski@yahoo.com>
2020-07-07 13:38:50 -0400

    clean up code

--- src/main.js
+++ src/main.js
@@ -1,22 +1,21 @@
-/* eslint-disable */
 const buttons = document.getElementById('buttons');
-const adviceTime = document.createElement("p");
+const adviceTime = document.createElement('p');
 buttons.after(adviceTime);
 
 buttons.onclick = function giveAdvice() {
   fetch('https://api.adviceslip.com/advice')
-  .then(response => {
-    if (response.status === 200) {
-      return response.json();
-      //return information if response status is 200
-    }
-    throw new Error('Resource not found');
-    //throw error if response status fails
-  })
-  .then(data => {
-    let adviceReq = data.slip.advice;
-    //set advice to variable for ease
-    adviceTime.textContent=adviceReq;
-    //display advice on page
-  })
-}
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      // return information if response status is 200
+      }
+      throw new Error('Resource not found');
+    // throw error if response status fails
+    })
+    .then((data) => {
+      const adviceReq = data.slip.advice;
+      // set advice to variable for ease
+      adviceTime.textContent = adviceReq;
+    // display advice on page
+    });
+};
