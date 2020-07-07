cec8257c71f01b9c51e4423f2807b249ac75803a
Jenna Koslowski <jenna.koslowski@yahoo.com>
2020-07-07 12:51:54 -0400

    remove unnecessary code

--- src/main.js
+++ src/main.js
@@ -3,43 +3,16 @@ const buttons = document.getElementById('buttons');
 const adviceTime = document.createElement("p");
 buttons.after(adviceTime);
 
-/* const requestURL = 'https://api.adviceslip.com/advice';
-const request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-
- buttons.onclick = function giveAdvice() {
-  const advice = request.response;
-  const adviceSpace = document.createElement('p');
-  adviceSpace.textContent = advice['slip.advice'];
-  adviceSpace.appendChild(adviceSpace);
-}; */
-
-/* const data = {};
-  fetch('https://api.adviceslip.com/advice', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  }).then((res) => res.json()).then((datas) => alert(datas)).catch(err => {
-      console.log('rejected: ', err);
-    });; */
-
 buttons.onclick = function giveAdvice() {
   fetch('https://api.adviceslip.com/advice')
   .then(response => {
-    console.log('Resolved: ', response);
     if (response.status === 200) {
       return response.json();
     }
     throw new Error('Resource not found');
   })
   .then(data => {
-    console.log(data);
     let adviceReq = data.slip.advice;
-    console.log(adviceReq)
     adviceTime.textContent=adviceReq;
   })
 }
