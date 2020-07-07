1d4481b6834301f0a6666145620c85cf5cf4b576
Jenna Koslowski <jenna.koslowski@yahoo.com>
2020-07-07 13:14:47 -0400

    add comment about display

--- src/main.js
+++ src/main.js
@@ -7,12 +7,16 @@ buttons.onclick = function giveAdvice() {
   fetch('https://api.adviceslip.com/advice')
   .then(response => {
     if (response.status === 200) {
-      return response.json(); //return information if response status is 200
+      return response.json();
+      //return information if response status is 200
     }
-    throw new Error('Resource not found'); //throw error if response status fails
+    throw new Error('Resource not found');
+    //throw error if response status fails
   })
   .then(data => {
-    let adviceReq = data.slip.advice; //set advice to variable for ease
+    let adviceReq = data.slip.advice;
+    //set advice to variable for ease
     adviceTime.textContent=adviceReq;
+    //display advice on page
   })
 }
